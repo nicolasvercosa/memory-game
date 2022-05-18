a35873e17b2c93eea577a6efb56bf2d3f21665f3
Nicolas Verçosa <vercosanicolas@gmail.com>
2022-05-18 09:10:46 -0300

    primeiro commit

---
+++ script.js
@@ -0,0 +1,11 @@
+
+const start = () => {
+    tableMaker()
+}
+
+const tableMaker = () => {
+    const table = document.getElementById("table");
+    const newTd = document.createElement("td");
+
+    
+}
